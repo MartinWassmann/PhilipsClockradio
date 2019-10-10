fe7c1981299b8069c13ee41eb5dea6d3031cb9d7
Martin <martingw@live.dk>
2019-10-10 15:35:18 +0200

    Galgeleg Projekt 62550

---
+++ app/src/main/java/com/example/galgelegs185033/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.galgelegs185033;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button A = (Button)findViewById(R.id.buttonA);
+        A.setOnClickListener(this);
+
+        Button B = (Button)findViewById(R.id.buttonB);
+        B.setOnClickListener(this);
+
+        Button C = (Button)findViewById(R.id.buttonC);
+        C.setOnClickListener(this);
+
+        Button D = (Button)findViewById(R.id.buttonD);
+        D.setOnClickListener(this);
+
+        Button E = (Button)findViewById(R.id.buttonE);
+        E.setOnClickListener(this);
+
+        Button F = (Button)findViewById(R.id.buttonF);
+        F.setOnClickListener(this);
+
+        Button G = (Button)findViewById(R.id.buttonG);
+        G.setOnClickListener(this);
+
+        Button H = (Button)findViewById(R.id.buttonH);
+        H.setOnClickListener(this);
+
+        Button I = (Button)findViewById(R.id.buttonI);
+        I.setOnClickListener(this);
+
+        Button J = (Button)findViewById(R.id.buttonJ);
+        J.setOnClickListener(this);
+
+        Button K = (Button)findViewById(R.id.buttonK);
+        K.setOnClickListener(this);
+
+        Button L = (Button)findViewById(R.id.buttonL);
+        L.setOnClickListener(this);
+
+        Button M = (Button)findViewById(R.id.buttonM);
+        M.setOnClickListener(this);
+
+        Button N = (Button)findViewById(R.id.buttonN);
+        N.setOnClickListener(this);
+
+        Button O = (Button)findViewById(R.id.buttonO);
+        O.setOnClickListener(this);
+
+        Button P = (Button)findViewById(R.id.buttonP);
+        P.setOnClickListener(this);
+
+        Button Q = (Button)findViewById(R.id.buttonQ);
+        Q.setOnClickListener(this);
+
+        Button Rr = (Button)findViewById(R.id.buttonR);
+        Rr.setOnClickListener(this);
+
+        Button S = (Button)findViewById(R.id.buttonS);
+        S.setOnClickListener(this);
+
+        Button T = (Button)findViewById(R.id.buttonT);
+        T.setOnClickListener(this);
+
+        Button U = (Button)findViewById(R.id.buttonU);
+        U.setOnClickListener(this);
+
+        Button V = (Button)findViewById(R.id.buttonV);
+        V.setOnClickListener(this);
+
+        Button X = (Button)findViewById(R.id.buttonX);
+        X.setOnClickListener(this);
+
+        Button Y = (Button)findViewById(R.id.buttonY);
+        Y.setOnClickListener(this);
+
+        Button Z = (Button)findViewById(R.id.buttonZ);
+        Z.setOnClickListener(this);
+
+        Button AE = (Button)findViewById(R.id.buttonAE);
+        AE.setOnClickListener(this);
+
+        Button OE = (Button)findViewById(R.id.buttonOE);
+        OE.setOnClickListener(this);
+
+        Button AA = (Button)findViewById(R.id.buttonAA);
+        AA.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) { //TODO Implement
+
+    }
+}
